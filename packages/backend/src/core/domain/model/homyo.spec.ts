1af8ab2e8d1a303280e3b691c4b4beb2f8d5e4e3
ss49919201 <sakashin10291029@gmail.com>
2024-11-10 08:51:05 +0900

    unit tests

--- packages/backend/src/core/domain/model/homyo.spec.ts
+++ packages/backend/src/core/domain/model/homyo.spec.ts
@@ -12,4 +12,48 @@ describe("newHomyo", () => {
     // Assert
     expect(homyo).toEqual(props);
   });
+
+  it("should throw an error if id is not positive", () => {
+    // Arrange
+    const props = { id: 0, name: "homyo" };
+
+    // Act
+    const act = () => newHomyo(props);
+
+    // Assert
+    expect(act).toThrowError();
+  });
+
+  it("should throw an error if id is not int", () => {
+    // Arrange
+    const props = { id: 1.1, name: "homyo" };
+
+    // Act
+    const act = () => newHomyo(props);
+
+    // Assert
+    expect(act).toThrowError();
+  });
+
+  it("should throw an error if name is empty", () => {
+    // Arrange
+    const props = { id: 1, name: "" };
+
+    // Act
+    const act = () => newHomyo(props);
+
+    // Assert
+    expect(act).toThrowError();
+  });
+
+  it("should throw an error if name is too long", () => {
+    // Arrange
+    const props = { id: 1, name: "a".repeat(256) };
+
+    // Act
+    const act = () => newHomyo(props);
+
+    // Assert
+    expect(act).toThrowError();
+  });
 });
