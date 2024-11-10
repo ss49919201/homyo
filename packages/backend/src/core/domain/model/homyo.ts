304e84a15940cb35e26102aad729f9055424a7ed
ss49919201 <sakashin10291029@gmail.com>
2024-11-10 18:54:47 +0900

    homyo name brand

--- packages/backend/src/core/domain/model/homyo.ts
+++ packages/backend/src/core/domain/model/homyo.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const homyoSchema = z
   .object({
     id: z.number().positive().int().min(1).brand("HomyoId"),
-    name: z.string().min(1).max(255),
+    name: z.string().min(1).max(255).brand("HomyoName"),
   })
   .readonly();
 
