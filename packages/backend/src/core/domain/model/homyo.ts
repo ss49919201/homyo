5f89666c54811596f53d5640fb43b55f090d8c0f
ss49919201 <sakashin10291029@gmail.com>
2024-11-12 15:03:29 +0900

    homyo name validation

--- packages/backend/src/core/domain/model/homyo.ts
+++ packages/backend/src/core/domain/model/homyo.ts
@@ -17,3 +17,10 @@ const newHomyo = (props: { id: ModelId; name: string }): Homyo => {
 export const Homyo = {
   new: newHomyo,
 };
+
+export const validateHomyoLength = (name: string): Error | null => {
+  if (name.length !== 2) {
+    return new Error("Homyo name must be 2 characters length");
+  }
+  return null;
+};
