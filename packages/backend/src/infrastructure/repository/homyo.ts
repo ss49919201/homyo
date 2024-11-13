e66cbaa8919b67a47d2c64323cb44c997e47eb23
ss49919201 <sakashin10291029@gmail.com>
2024-11-13 13:13:58 +0900

    homyo repository

---
+++ packages/backend/src/infrastructure/repository/homyo.ts
@@ -0,0 +1,20 @@
+import { Homyo } from "../../core/domain/model/homyo";
+import { ModelId } from "../../core/domain/model/id";
+import { HomyoRepository } from "../../core/domain/repository/homyo";
+
+const homyos = new Map<ModelId, Homyo>();
+
+const save = async (homyo: Homyo): Promise<void> => {
+  homyos.set(homyo.id, homyo);
+};
+
+const loadById = async (id: ModelId): Promise<Homyo | null> => {
+  return homyos.get(id) ?? null;
+};
+
+export const newHomyoRepository = (): HomyoRepository => {
+  return {
+    save,
+    loadById,
+  };
+};
