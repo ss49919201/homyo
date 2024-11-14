6bab2103da13f18db8b147cb6c561bde90a91ae9
ss49919201 <sakashin10291029@gmail.com>
2024-11-14 17:49:57 +0900

    string to model id

--- packages/backend/src/core/domain/model/id.ts
+++ packages/backend/src/core/domain/model/id.ts
@@ -5,3 +5,8 @@ export type ModelId = ReturnType<typeof crypto.randomUUID>;
 export const newModelId = (): ModelId => {
   return crypto.randomUUID();
 };
+
+export const stringToModelId = (s: string): ModelId => {
+  // FIXME: Validate the string is a valid UUID
+  return s as ModelId;
+};
